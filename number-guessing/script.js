b102f7e096500a11e25e507793adab98ce59acca
Chellah-nawel <chellah.nawel@gmail.com>
2026-06-16 23:04:47 +0100

    adding the number of attempts

--- number-guessing/script.js
+++ number-guessing/script.js
@@ -1,14 +1,14 @@
 let rand_number= Math.floor(Math.random()*101);
-
+let attempts=0;
 let number= -1;
 
 while(number != rand_number){
     number= window.prompt("Enter a number between 0 and 100");
-
+    attempts++;
     if (number > rand_number){
         window.alert("Too Big, try another one");
-
+        attempts++;
     } else if (number < rand_number){
         window.alert("Too Low, try again!");} 
-        else window.alert("You did it it is the right number");
+        else window.alert(`You did it it is the right number, with ${attempts} attempts`);
 }
