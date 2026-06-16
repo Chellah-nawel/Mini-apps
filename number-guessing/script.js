47062856adbfb80a82f38761591bd93a88fcdbe5
Chellah-nawel <chellah.nawel@gmail.com>
2026-06-16 23:14:07 +0100

    modifying the loop condition

--- number-guessing/script.js
+++ number-guessing/script.js
@@ -1,8 +1,8 @@
 let rand_number= Math.floor(Math.random()*101);
 let attempts=0;
-let number= -1;
-
-while(number != rand_number){
+let number;
+let running =true;
+while(running){
     number= window.prompt("Enter a number between 0 and 100");
     if(isNaN(number)){
         window.alert("Please enter a valid number");
@@ -12,9 +12,9 @@ while(number != rand_number){
     }
     attempts++;
     if (number > rand_number){
-        window.alert("Too Big, try another one");
+        window.alert("Too Big, try another one ^^(");
         attempts++;
     } else if (number < rand_number){
-        window.alert("Too Low, try again!");} 
-        else window.alert(`You did it it is the right number, with ${attempts} attempts`);
+        window.alert("Too Low, try again !!)");} 
+        else window.alert(`You did it, it's the right number, it took you ${attempts} attempts`);
 }
