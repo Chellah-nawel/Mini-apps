f7c618603c93eb93af194320cc4871db203690c4
Chellah-nawel <chellah.nawel@gmail.com>
2026-06-16 23:01:40 +0100

    fixing the problem of non integer number

--- number-guessing/script.js
+++ number-guessing/script.js
@@ -1,13 +1,14 @@
-let rand_number= Math.random()*101
+let rand_number= Math.floor(Math.random()*101);
 
-let number= window.prompt("Enter a number between 0 and 100");
+let number= -1;
 
 while(number != rand_number){
     number= window.prompt("Enter a number between 0 and 100");
-    
+
     if (number > rand_number){
         window.alert("Too Big, try another one");
-    } else number < rand_number ? window.alert("Too Low, try again!") : window.alert("You did it it is the right number");
 
-    
+    } else if (number < rand_number){
+        window.alert("Too Low, try again!");} 
+        else window.alert("You did it it is the right number");
 }
