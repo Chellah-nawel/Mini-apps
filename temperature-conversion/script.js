a6e349185bf02f96b7e3811f878f12c186fa3374
Chellah-nawel <chellah.nawel@gmail.com>
2026-06-17 23:48:36 +0100

    adding the link

--- temperature-conversion/script.js
+++ temperature-conversion/script.js
@@ -1,7 +1,8 @@
-let temperature = document.getElementById("input-field")
+let temperature = document.getElementById("input-field");
 let to_fahrenheit = document.getElementById("to-fahrenheit");
 let to_celsius = document.getElementById("to-celsius");
 let result = document.getElementById("result");
+let btn_submit = document.getElementById("btn-submit");
 
 function toCelsius(temperature){
     return (temperature -32) * (5/9);
@@ -13,10 +14,12 @@ function toFahrenheit(temperature){
 
 function convert(){
     temperature= Number(temperature.value);
-    if(to-fahrenheit.checked){
+    if(to_fahrenheit.checked){
         result.textContent= (toFahrenheit(temperature)).toFixed + "F";
-    } else if (to-celsius.checked){
+    } else if (to_celsius.checked){
         result.textContent= (toCelsius(temperature).toFixed + "C");
     }
     else alert("Please select a unit");
 }
+
+btn_submit.addEventListener("click", convert);
