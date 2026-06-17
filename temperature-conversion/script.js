5c42646064d918dd624ac26dfed7264137bd62b0
Chellah-nawel <chellah.nawel@gmail.com>
2026-06-17 23:30:58 +0100

    declaring variables

---
+++ temperature-conversion/script.js
@@ -0,0 +1,4 @@
+let temperature = document.getElementById("input-field")
+let to_fahrenheit = document.getElementById("to-fahrenheit");
+let to_celsius = document.getElementById("to-celsius");
+let result = document.getElementById("result");
