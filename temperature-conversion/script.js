c7ed27cd954848e31912999c556cd625d4c43f7b
Chellah-nawel <chellah.nawel@gmail.com>
2026-06-17 23:41:57 +0100

    declaring toFahrenheit function

--- temperature-conversion/script.js
+++ temperature-conversion/script.js
@@ -3,6 +3,20 @@ let to_fahrenheit = document.getElementById("to-fahrenheit");
 let to_celsius = document.getElementById("to-celsius");
 let result = document.getElementById("result");
 
-function to_celsius(temperature){
+function toCelsius(temperature){
     return (temperature -32) * (5/9);
+}
+
+function toFahrenheit(temperature){
+    return temperature * (9/5) + 32;
+}
+
+function convert(){
+    temperature= Number(temperature.value);
+    if(to-fahrenheit.checked){
+        result.textContent= (toFahrenheit(temperature)).toFixed + "F";
+    } else if (to-celsius.checked){
+        result.textContent= (toCelsius(temperature).toFixed + "C");
+    }
+    else alert("Please select a unit");
 }
