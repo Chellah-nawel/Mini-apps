5e86be2d4f3cf87b822c245a3b1c2f94975816f0
Chellah-nawel <chellah.nawel@gmail.com>
2026-06-17 23:34:57 +0100

    declaring to-celsius function

--- temperature-conversion/script.js
+++ temperature-conversion/script.js
@@ -2,3 +2,7 @@ let temperature = document.getElementById("input-field")
 let to_fahrenheit = document.getElementById("to-fahrenheit");
 let to_celsius = document.getElementById("to-celsius");
 let result = document.getElementById("result");
+
+function to_celsius(temperature){
+    return (temperature -32) * (5/9);
+}
