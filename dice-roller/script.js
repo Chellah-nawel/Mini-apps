611bce70656800068330be0a807e1ff85f8624ca
Chellah-nawel <chellah.nawel@gmail.com>
2026-06-19 21:24:07 +0100

    fixing printing problem

--- dice-roller/script.js
+++ dice-roller/script.js
@@ -1,24 +1,40 @@
 let input = document.getElementById("input-field");
 let btn = document.getElementById("submit-btn");
 let result = document.getElementById("result");
-let image_container = document.getElementById("image");
+let image_container = document.getElementById("images");
+let container = document.getElementById("container");
 
 const images= ["images/dice.png", "images/dice (1).png", "images/dice (2).png", "images/dice (3).png", "images/dice (4).png", "images/dice (5).png"];
 let dices;
 let numbers=[];
-let displayed_images=["-1"];
+let displayed_images=[];
 
 function roll_dices(){
-    dices= Number(input.Value);
+    dices= Number(input.value);
+    numbers=[];
+    displayed_images=[];
     for(let i=1; i<= dices; i++){
-        dice_face= Math.floor(Math.random()*6)+1;
+        let dice_face= Math.floor(Math.random()*6)+1;
         numbers.push(dice_face);
         displayed_images.push(images[dice_face -1]);
     }
 
-    result.textContent(numbers);
-    image_container.textContent(displayed_images);
+    result.textContent=numbers.join(",");
+    image_container.innerHTML = "";
 
+    for (let imgPath of displayed_images) {
+
+        if(displayed_images.length>4){
+            container.style.height= "550px";
+        }
+
+        const img = document.createElement("img");
+
+        img.src = imgPath;
+        img.width = 100;
+
+        image_container.appendChild(img);
+    }
 }
 
 btn.addEventListener("click", roll_dices);
