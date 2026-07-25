80e3118fd1aa44710cd2c40865b64c86769a9221
Chellah-nawel <chellah.nawel@gmail.com>
2026-07-25 21:20:29 +0100

    declaring var and event listners

---
+++ stopwatch/script.js
@@ -0,0 +1,23 @@
+//var
+let time= document.getElementById("watch_container");
+let btn_start = document.getElementById("btn_start");
+let btn_stop = document.getElementById("btn_stop");
+let btn_reset = document.getElementById("btn_reset");
+
+//functions
+function start(){
+
+}
+
+function stop(){
+
+}
+
+function reset(){
+
+}
+
+//event listners
+btn_start.addEventListener('click', start);
+btn_stop.addEventListener('click', stop);
+btn_reset.addEventListener('click', reset);
